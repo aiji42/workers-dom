4fbd07254a016ac0f1414e29569cf7bee5187752
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2024-04-02 11:17:02 +0900

    add linkedom and @mozilla/readability

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,22 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { parseHTML } from 'linkedom';
+import { Readability } from '@mozilla/readability';
 
-export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
-}
+export interface Env {}
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		const url = new URL(request.url);
+		const target = url.searchParams.get('url');
+		if (!target) return new Response('url is required', { status: 400 });
+
+		const html = await fetch(target).then((res) => res.text());
+		const { document } = parseHTML(html);
+
+		const reader = new Readability(document);
+		const article = reader.parse();
+		if (!article) return new Response('failed to parse article', { status: 500 });
+
+		console.log(article);
+		return Response.json(article);
 	},
 };
